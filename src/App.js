a7cc296f9d6a19f333fe3c995a9755a4170bd494
Daniel Pernia <danieleduardo0019@gmail.com>
2026-08-13 13:25:34 -0500

    Update table selection display format

--- src/App.js
+++ src/App.js
@@ -394,7 +394,7 @@ function Reservation({ onAddBooking, initialSede = '' }) {
               <option value="">{selectedSede ? (availableTables.length ? 'Selecciona una mesa' : 'No hay mesas disponibles en esta sede') : 'Selecciona primero una sede'}</option>
               {availableTables.map((table) => (
                <option key={table.idmesa} value={table.idmesa}>
-                  {table.nombremesa ? table.nombremesa : `Mesa ${table.idmesa}`}
+                  {`Mesa ${table.idmesa} (${table.cantidadsillas || 4} sillas)`}
                 </option>
               ))}
             </select>
